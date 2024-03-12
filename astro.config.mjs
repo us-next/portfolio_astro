ce6d11579b4f5740c2ab7ad795c4df721a121a61
us-next <uno.s@next-japan.co.jp>
2024-03-12 15:28:30 +0900

    resetCSSの追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        //scssファイルの読み込み
+        scss: {
+          additionalData: `@use "src/css/reset.scss" as *;`
+        }
+      }
+    }
+  }
+});
