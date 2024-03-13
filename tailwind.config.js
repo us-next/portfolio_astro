ef649af3f262b0e95a664e68560f4df0fb8acaaf
us-next <uno.s@next-japan.co.jp>
2024-03-13 14:01:36 +0900

    reactとtailwindcssの追加

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+  purge: ['./src/**/*.{astro,js,jsx,ts,tsx,vue}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
