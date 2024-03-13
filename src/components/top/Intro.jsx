c93e0be78d3bb43fa2a13f85ceb49ee1addf4a64
us-next <uno.s@next-japan.co.jp>
2024-03-13 14:02:46 +0900

    コンポーネントをreactファイルに変更

---
+++ src/components/top/Intro.jsx
@@ -0,0 +1,28 @@
+export function Intro({ }) {
+  return (
+    <section class="intro">
+      <h2>intro</h2>
+      <p class="intro__txt">
+        Steasy ist ein mobiler Mini-Steamer mit eingebautem Akku. Damit kannst
+        du deinen vorgekochten Lunch überall hochwertig aufwärmen.
+      </p>
+      <div>
+        <p class="intro__pic intro__pic01">
+          <img src="/public/img/intro_01.jpg" alt="intro01" />
+        </p>
+        <p class="intro__pic intro__pic02">
+          <img src="/public/img/intro_02.jpg" alt="intro02" />
+        </p>
+        <p class="intro__pic intro__pic03">
+          <img src="/public/img/intro_03.jpg" alt="intro03" />
+        </p>
+        <p class="intro__pic intro__pic04">
+          <img src="/public/img/intro_04.jpg" alt="intro04" />
+        </p>
+        <p class="intro__pic intro__pic05">
+          <img src="/public/img/intro_05.jpg" alt="intro05" />
+        </p>
+      </div>
+    </section>
+  );
+}
