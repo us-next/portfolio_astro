15821c8b7b0f9b6217c93097c4413c0692ed89d9
us-next <uno.s@next-japan.co.jp>
2024-03-15 14:31:44 +0900

    スマホ版完成

---
+++ src/components/top/About.jsx
@@ -0,0 +1,26 @@
+import { Img } from './Img';
+
+export function About({ }) {
+  return (
+    <section className="py-14">
+      <h2 className="hidden">about</h2>
+      <div className="relative mb-40 pb-14">
+        <div className="absolute top-[30px] w-[82%] h-[386px] bg-[#487B8B] rounded-2xl"></div>
+        <p className="relative z-10 w-[82%] h-[372px] mx-auto">
+          <Img src="/public/img/intro_05.jpg" alt="about" />
+        </p>
+        <p className="absolute bottom-0  right-0 z-20 w-[55%]">
+          <Img src="/public/img/intro_03.jpg"/>
+        </p>
+      </div>
+      <p className="mb-4 px-6 text-2xl font-medium">
+        Warum solltest du deinen Lunch steamen? Es schmeckt einfach besser.
+      </p>
+      <p className="mb-6 px-6">
+        Endlich eine Art des Aufwärmens, die der Qualität deiner Mealprep- Gerichte
+        entspricht. Unsere LPS Technologie sorgt für ein schonendes Aufwärmen, um
+        Geschmack und Nährstoffe bestmöglich zu erhalten.
+      </p>
+    </section>
+  )
+}
