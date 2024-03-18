d8932cf7836c050d0ebef0ac9239a4a45d2b14d2
us-next <uno.s@next-japan.co.jp>
2024-03-18 11:43:29 +0900

    classを呼び出し先で指定

--- src/components/top/WorkCont.jsx
+++ src/components/top/WorkCont.jsx
@@ -1,17 +1,23 @@
 import { Img } from "./Img";
 import { Btn } from "../btn";
 
-export function WorkCont({src, alt, title, type, desc}) {
+export function WorkCont({src, alt, title, type, desc, padding, picPos, shadowX, shadowY}) {
+  //呼び出し先で指定
+  //padding： 画像とシャドウの箱に使用
+  //picPos： 画像右寄せ時に使用
+  //shadowX : シャドウの左右位置
+  //shadowY : シャドウの上下位置
+  
   return (
-    <div>
-      <div className="relative mb-7 pt-8">
-        <div className="absolute top-0 w-[87%] h-[351px] bg-[#315B6D] rounded-xl"></div>
-        <p className="relative z-10 w-[87%] h-[351px] ml-auto"><Img src={src} alt={alt}/></p>
+    <div className="mb-14">
+      <div className={`relative mb-7 ${padding}`}>
+        <div className={`absolute ${shadowX} ${shadowY} w-[87%] h-[351px] bg-[#315B6D] rounded-xl`}></div>
+        <p className={`relative z-10 w-[87%] h-[351px] ${picPos}`}><Img src={src} alt={alt}/></p>
       </div>
         <p className="mb-4 text-2xl font-semibold">{title}</p>
-        <p className="mb-4 text-[#12354A] text-lg font-medium">{type}</p>
+        <p className="mb-4 text-[#12354A] text-lg font-inter font-medium">{type}</p>
         <p className="mb-7">{desc}</p>
-        <Btn size="small" text="More" />
+        <Btn size="w-24" text="More" />
     </div>
   )
 }
--- src/components/top/Works.jsx
+++ src/components/top/Works.jsx
@@ -4,12 +4,31 @@ import { WorkCont } from "./WorkCont";
 export function Works({ }) {
   return (
     <section className="px-6">
-      <h2 className="mb-9 text-2xl">Works</h2>
+      <h2 className="mb-9 font-inter text-2xl">Works</h2>
       <WorkCont
         src="/public/img/intro_01.jpg"
         title="生徒評価アプリ"
         type="App" 
-        desc="Next.jsの練習を兼ねた仮想の生徒評価アプリ"/>
+        desc="Next.jsの練習を兼ねた仮想の生徒評価アプリ"
+        padding="pt-8"
+        picPos="ml-auto"
+        shadowY="top-0"/>
+      <WorkCont
+        src="/public/img/intro_01.jpg"
+        title="仮想Webサイト"
+        type="Website" 
+        desc="Vue.jsで作成した仮想Webサイト"
+        padding="pb-8"
+        shadowX="right-0"
+        shadowY="top-8"/>
+      <WorkCont
+        src="/public/img/intro_01.jpg"
+        title="ポートフォリオ"
+        type="Website" 
+        desc="AstroとReactを使用し作成"
+        padding="pt-8"
+        shadowX="right-0"
+        shadowY="top-0"/>
     </section>
   )
 }
