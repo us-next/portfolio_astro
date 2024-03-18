cbfbbe5f1aaec07b6d4e04ccb7915f407522db86
us-next <uno.s@next-japan.co.jp>
2024-03-18 11:42:20 +0900

    クラスの指定方法変更

--- src/components/btn.jsx
+++ src/components/btn.jsx
@@ -1,13 +1,7 @@
-//ボタン横幅 呼び出し時にサイズ指定
-const widthClasses = {
-  small: 'w-24'
-}
-
 export function Btn({size, text}) {
-  const widthClass = widthClasses[size];
   
   return (
-    <p className={widthClass}>
+    <p className={`${size} font-inter`}>
       <a href="#" className="block py-4 bg-black text-white text-center leading-none rounded-lg">{text}</a>
     </p>
   )
