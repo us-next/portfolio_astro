44867b8c90ba1ae57bdb6d364dd2b01e08e1a6b4
us-next <uno.s@next-japan.co.jp>
2024-03-21 14:14:09 +0900

    スキルセクション作成

---
+++ src/components/top/Skill.jsx
@@ -0,0 +1,59 @@
+import {SkillCont} from "./SkillCont"
+
+export function Skill({}) {
+  return (
+    <section className="flex flex-wrap justify-between gap-y-11 px-[6%] py-20 bg-[#12354A] text-white">
+      <h2 className="hidden">skill</h2>
+      <SkillCont 
+        src="/public/img/skill_html.svg"
+        alt="HTML"
+        skillName="HTML"
+        skillDesc="BEMの命名規則を使用"
+      />
+      <SkillCont 
+        src="/public/img/skill_css.svg"
+        alt="CSS"
+        skillName="CSS3"
+        skillDesc="Dartsassを使用"
+      />
+      <SkillCont 
+        src="/public/img/skill_js.svg"
+        alt="JavaScript"
+        skillName="JavaScript"
+        skillDesc="複雑な動きも実装可能"
+      />
+      <SkillCont 
+        src="/public/img/skill_react.svg"
+        alt="React"
+        skillName="React"
+        skillDesc="簡単なアプリ制作が可能"
+      />
+      <SkillCont 
+        src="/public/img/skill_vue.svg"
+        alt="Vue3"
+        skillName="Vue3"
+        skillDesc="簡単なアプリ制作が可能"
+      />
+      <SkillCont 
+        src="/public/img/skill_astro.svg"
+        alt="Astro"
+        skillName="Astro"
+        skillDesc="Webサイト制作が可能"
+      />
+      <SkillCont 
+        src="/public/img/skill_figma.svg"
+        alt="Figma"
+        skillName="Figma"
+        skillDesc="デザインカンプからの"
+        skillDescNext="コーディングが可能"
+        />
+      <SkillCont 
+        src="/public/img/skill_xd.svg"
+        alt="XD"
+        skillName="XD"
+        skillDesc="デザインカンプからの"
+        skillDescNext="コーディングが可能"
+      />
+    </section>
+  )
+}
---
+++ src/components/top/SkillCont.jsx
@@ -0,0 +1,13 @@
+import { SquareImg } from "./SquareImg";
+
+export function SkillCont({ src, alt, skillName, skillDesc, skillDescNext }) {
+  return (
+    <div className="w-[48%] text-center">
+      <div className="flex justify-center items-center mx-auto mb-5 bg-pink w-[107px] h-[107px] rounded-full">
+        <p className="w-[60px] aspect-square"><SquareImg src={src} alt={alt} /></p>
+      </div>
+      <p className="mb-1 font-inter text-lg">{skillName}</p>
+      <p className="text-xs">{skillDesc}<br />{skillDescNext}</p>
+    </div>
+  )
+}
