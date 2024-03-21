1428cfbeea39a213e51114b7765113fa88ccb50d
us-next <uno.s@next-japan.co.jp>
2024-03-21 14:13:32 +0900

    画像のコンポーネント名変更

--- src/components/top/Intro.jsx
+++ src/components/top/Intro.jsx
@@ -1,8 +1,8 @@
-import { Img } from './Img';
+import { RoundedImg } from './RoundedImg';
 
 export function Intro({ }) {
   return (
-    <section className="py-14 font-inter">
+    <section className="px-[6.4%] py-14 font-inter">
       <h2 className="hidden">intro</h2>
       <p className="w-11/12 mx-auto mb-14 text-center text-2xl font-medium">
         Steasy ist ein mobiler Mini-Steamer mit eingebautem Akku. Damit kannst
@@ -10,19 +10,19 @@ export function Intro({ }) {
       </p>
       <div className="grid grid-rows-11 grid-cols-2 gap-16px">
         <p className="col-start-1 col-end-2 row-start-2 row-end-8">
-          <Img src="/public/img/intro_01.jpg" alt="intro01" />
+          <RoundedImg src="/public/img/intro_01.jpg" alt="intro01" />
         </p>
         <p className="col-start-2 col-end-3 row-start-1 row-end-4">
-          <Img src="/public/img/intro_02.jpg" alt="intro02" />
+          <RoundedImg src="/public/img/intro_02.jpg" alt="intro02" />
         </p>
         <p className="col-start-1 col-end-2 row-start-8 row-end-11">
-          <Img src="/public/img/intro_03.jpg" alt="intro03" />
+          <RoundedImg src="/public/img/intro_03.jpg" alt="intro03" />
         </p>
         <p className="col-start-2 col-end-3 row-start-9 row-end-12">
-          <Img src="/public/img/intro_04.jpg" alt="intro04" />
+          <RoundedImg src="/public/img/intro_04.jpg" alt="intro04" />
         </p>
         <p className="col-start-2 col-end-3 row-start-4 row-end-9">
-          <Img src="/public/img/intro_05.jpg" alt="intro05" />
+          <RoundedImg src="/public/img/intro_05.jpg" alt="intro05" />
         </p>
       </div>
     </section>
