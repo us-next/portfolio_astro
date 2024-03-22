11d5122b4b203febc478afd7ec442a9a38896e44
us-next <uno.s@next-japan.co.jp>
2024-03-22 18:16:26 +0900

    pc版hidden

--- src/components/BurgerButton.jsx
+++ src/components/BurgerButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function BurgerButton({ onClick }) {
   return (
-    <button onClick={onClick} className="relative z-10 flex justify-center items-center w-6 h-6">
+    <button onClick={onClick} className="lg:hidden relative z-10 flex justify-center items-center w-6 h-6">
       <span className="absolute translate-y-[-5px] block w-3.5 h-px bg-black"></span>
       <span className="absolute block w-3.5 h-px bg-black"></span>
       <span className="absolute translate-y-[5px] block w-3.5 h-px bg-black"></span>
