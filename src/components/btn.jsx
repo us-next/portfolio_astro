e574d4490087cba55575dde1259d18fd35ee3df6
us-next <uno.s@next-japan.co.jp>
2024-06-02 15:02:14 +0900

    target属性追加

--- src/components/btn.jsx
+++ src/components/btn.jsx
@@ -1,8 +1,8 @@
-export function Btn({size, text}) {
+export function Btn({size, href, target, text}) {
   
   return (
     <p className={`${size} font-inter`}>
-      <a href="#" className="block py-4 bg-black text-white text-center text-sm leading-none rounded-lg transition duration-300 hover:scale-90">{text}</a>
+      <a href={href} className="block py-4 bg-black text-white text-center text-sm leading-none rounded-lg transition duration-300 hover:scale-90" target={target}>{text}</a>
     </p>
   )
 }
