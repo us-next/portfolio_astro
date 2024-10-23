78002d697e4d88e5c4b1be03ef29802e183cf9b8
us-next <uno.s@next-japan.co.jp>
2024-10-23 16:45:42 +0900

    文字色固定

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-scroll';
 
 export function HeaderNav({onClick, className}) {
   return (
-    <nav className={`absolute z-0 top-0 left-0 w-full rounded-2xl bg-white overflow-hidden duration-300 transition-all ${className}`}>
+    <nav className={`absolute z-0 top-0 left-0 w-full rounded-2xl bg-white text-black overflow-hidden duration-300 transition-all ${className}`}>
       <ul>
         <li><Link onClick={onClick} className="block mb-2 px-6 py-2 cursor-pointer" to="about" smooth={true}>About</Link></li>
         <li><Link onClick={onClick} className="block mb-2 px-6 py-2 cursor-pointer" to="works" smooth={true}>Works</Link></li>
